4c973717fd435518432ac9f363da25e7834d9e2c
Debbykalu <debbykalu@gmail.com>
2024-04-02 19:45:32 +0100

    commit

--- src/Modal.js
+++ src/Modal.js
@@ -65,9 +65,7 @@ return (
             You will need to agree to our terms and conditions All the Lorem Ipsum generators on 
             the Internet tend to repeat predefined chunks as necessary, making this the first true 
             generator on the Internet. It uses a dictionary of over 200 Latin words, combined with 
-            a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. 
-            The generated Lorem Ipsum is therefore always free from repetition, injected humour, or 
-            non-characteristic words etc.
+            a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable.
             </DialogContentText>
             </DialogContent>
         <DialogActions data-testid="button-wrap" className='button-wrap'>
